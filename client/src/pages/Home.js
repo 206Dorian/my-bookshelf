4e0bdb57b63c22c5e75bad26392d5f088113939c
Timothy Barnaby <tbarnaby1@gmail.com>
2023-09-06 15:53:30 -0500

    loging signup forms viasble

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,8 +1,13 @@
 import React from 'react'
-
+import Login from '../components/Login.js'
+import Signup from '../components/Signup.js'
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>Home
+    <Login/>
+    <Signup/>
+
+    </div>
   )
 }
 
