87276dde7ed46df9dca098a90a53f873121b85d9
Timothy Barnaby <tbarnaby1@gmail.com>
2023-10-03 16:16:16 -0500

    more managable view on the bookshelf

--- client/src/components/Bookshelf.js
+++ client/src/components/Bookshelf.js
@@ -1,23 +1,42 @@
-import React from 'react';
-import './Bookshelf.css'
+import React, { useState } from 'react';
+import './Bookshelf.css';
 
 const Bookshelf = ({ books }) => {
+  const [activeBook, setActiveBook] = useState(null);
+
+  const handleBookClick = (book) => {
+    if (activeBook === book) {
+      setActiveBook(null); // Close the active book view
+    } else {
+      setActiveBook(book); // Show the clicked book in full screen
+    }
+  };
+
   return (
-    <div>
-      <h2>Bookshelf</h2>
-      <div>
+    <div className="container mt-5" id="bookshelfContainer">
+      <h2 className="mb-4">Bookshelf</h2>
+      <div className="row">
         {books.map((entry, index) => (
-          <div className='BookEntry' key={index}>
-            <h3>{entry.book?.title}</h3>
-            <p>Author: {entry.book?.author}</p>
-            <p>First Sentence: {entry.book?.firstSentence}</p>
-            <p>ISBN: {entry.ISBN}</p>
-            <p>Placement: {entry.placement}</p>
+          <div 
+            className={`col-2 m-1 BookEntry ${activeBook === entry.book ? 'active' : ''}`} 
+            key={index} 
+            onClick={() => handleBookClick(entry.book)}
+          >
+            <div className="spine">{entry.book?.title}</div>
           </div>
         ))}
       </div>
+      {activeBook && (
+        <div className="activeBookOverlay">
+          <h3>{activeBook.title}</h3>
+          <p>Author: {activeBook.author}</p>
+          <p>First Sentence: {activeBook.firstSentence}</p>
+          <button onClick={() => setActiveBook(null)}>Close</button>
+        </div>
+      )}
     </div>
-  );
+);
+
 };
 
 export default Bookshelf;
