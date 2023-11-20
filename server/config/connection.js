0b7c150eccc0cc3ebb7aa2383f41f79801fc0e13
Dorian Birch <206dorian@gmail.com>
2023-11-20 14:44:58 -0600

    deleted firebase

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,20 +1,9 @@
 const mongoose = require('mongoose');
 mongoose.set('strictQuery', false);
 
-const isFirebaseEnvironment = process.env.GCLOUD_PROJECT !== undefined;  // Firebase Functions set GCLOUD_PROJECT environment variable
-
-const getMongoDBConnectionString = () => {
-  if (isFirebaseEnvironment) {
-    const functions = require('firebase-functions');
-    return functions.config().mongodb.connectionstring;
-  } else {
-    return process.env.YOUR_CONNECTION_STRING;
-  }
-}
-
 const connectToMongoDB = async () => {
   try {
-    const uri = getMongoDBConnectionString();
+    const uri = process.env.MONGODB_CONNECTION_STRING; // Rename the environment variable as needed
     await mongoose.connect(uri, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
